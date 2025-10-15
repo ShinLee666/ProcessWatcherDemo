baf772962e51a0c47102eef3693a78885dcfa65a
ShinLee666 <73521205+ShinLee666@users.noreply.github.com>
2025-10-15 17:52:20 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using 进程查看器.Views;
+using System.Windows;
+
+namespace 进程查看器
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindowView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
